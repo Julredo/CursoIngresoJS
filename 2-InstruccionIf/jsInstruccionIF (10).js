97681d2e84ae9087e5fa6218b13d0117ae6e5f7d
Julredo <jul3nred@hotmail.com>
2019-02-08 20:18:20 -0300

    Unidad 2 10 Completa

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,19 +1,22 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	var resultado = alert(Math.floor(Math.random() * 10) + 1);
 	
+	var resultado = (Math.floor(Math.random() * 10) + 1);
+	alert(resultado);
 	
-	if(resultado>=0 && resultado >=4){
+	if(resultado>=1 && resultado <=4)
+	{
 		alert("Vamos, la proxima se puede.");
-									}
-	
+	}	
 
-		else if(resultado>4 && resultado >=8){
+		else if(resultado>4 && resultado <=8)
+		{
 			alert("Aprobo");
-									}
+		}
 
-			else{
+			else if(resultado==9 || resultado ==10)
+			{
 				alert("Excelente.");
-									}
+			}
 }//FIN DE LA FUNCIÓN
