6f417c0a1e4b46e0ce87690e0d258ff89c4a2076
Julredo <jul3nred@hotmail.com>
2019-02-08 21:18:20 -0300

    Revisar 4 y hacer Recu

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
+var num1 = prompt("Ingrese el primer numero");
+var num2 = prompt("Ingrese el segundo numero");
+
+
+if(num1 == num2){
+resultado1 = num1+num2;
+alert(resultado1)
+                }
+                
+
+     else if(num1>num2){
+     num1 = parseInt(num1);
+     num2 = parseInt(num2);
+     resultado2 = num1 - num2;
+     alert(resultado2);
+           }
+
+           else if(num1<num2){
+            num1 = parseInt(num1);
+            num2 = parseInt(num2);
+            resultado3 = num1 + num2;
+            alert(resultado3);
+            if(resultado3 >10){
+                alert("la suma es "+resultado3+", es mayor a 10");
+                             }
+                            }
+                  
 
 }
