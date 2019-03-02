16587f9f79e00832ac7af027cf0b048451287930
alumno <alumno>
2019-03-01 22:08:42 -0300

    Ejerciciofinal
    Signed-off-by: alumno <alumno>

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -5,19 +5,48 @@ function mostrar() {
     var superficie;
     var confirma;
 
-    //while(confirma!=false){
-    do {
-        var pais = prompt("Ingrese un pais.");
+    var paisCien = 0;
+    var contador = 0;
+    var paisImpar=0;
+    var paisIgulaCien=0;
+    var paisSuperaCien=0;
 
 
-    } while (confirma != false);
-
-    do {
-        var superficie = prompt("Ingrese la superficie de ese pais en numeros.");
-        superficie = parseInt(superficie);
-    } while (isNaN(superficie))
+    while (confirma != false) {
+        contador++;
+        do {
+            var pais = prompt("Ingrese un pais.");
 
 
-    confirma = confirm("¿Desea agregar otro pais?")
+        } while (!isNaN(pais));
+
+        do {
+            var superficie = prompt("Ingrese la superficie de ese pais en numeros.");
+            superficie = parseInt(superficie);
+        } while (isNaN(superficie))
+
+        if (superficie < 100) {
+        paisCien++;
+        }
+
+        if(superficie%2>0){
+            paisImpar++
+        }
+
+
+        if(superficie==100){
+            paisIgulaCien++
+        }
+
+        if(superficie)
+
+        confirma = confirm("¿Desea agregar otro pais?")
+    }
+
+
+
+    alert("Los paises cuya superficie es menor a 100 son " + paisCien+". La cantidad de paises con superficie impar es "+paisImpar+ ". La cantidad de paises con superficie igual a 100 es "+paisIgulaCien);
+
 }
+
 
